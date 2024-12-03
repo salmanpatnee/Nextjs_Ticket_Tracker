bf63b41e52f83f99ce11dd8c9933cc0ed2c5f579
salmanpatnee <salmanpatni92@gmail.com>
2024-12-03 14:28:28 +0500

    Add priority select field in the new ticket form

--- app/tickets/create/page.tsx
+++ app/tickets/create/page.tsx
@@ -6,7 +6,18 @@ const CreateTicketPage = () => {
     <div className="max-w-xl space-y-5">
       <TextField.Root placeholder="Title" />
       <TextArea placeholder="Description" />
-      <Button>Add new issue</Button>
+      <div className="w-full">
+        <Select.Root size="2" defaultValue="MEDIUM">
+          <Select.Trigger placeholder="Priority"/>
+          <Select.Content>
+            <Select.Item value="LOW">Low</Select.Item>
+            <Select.Item value="MEDIUM">Medium</Select.Item>
+            <Select.Item value="HIGH">Hign</Select.Item>
+            <Select.Item value="CRITICAL">Critical</Select.Item>
+          </Select.Content>
+        </Select.Root>
+      </div>
+      <Button>Add new ticket</Button>
     </div>
   );
 };
