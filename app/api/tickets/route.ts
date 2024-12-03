7d48f47957336ca1fc14f639b6f9c103901953d1
salmanpatnee <salmanpatni92@gmail.com>
2024-12-03 17:01:18 +0500

    Handle error

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const createTicketSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1),
+    title: z.string().min(1, "Title is required.").max(255),
+    description: z.string().min(1, 'Description is required.'),
     priority: z.enum(["LOW", "MEDIUM", "HIGH", "CRITICAL"]),
 });
 
@@ -15,7 +15,7 @@ export const POST = async (request: NextRequest) => {
     const validation = createTicketSchema.safeParse(body);
 
     if (!validation.success) {
-        return NextResponse.json(validation.error.errors, { status: 400 });
+        return NextResponse.json(validation.error.format(), { status: 400 });
     }
 
     try {
--- app/tickets/create/page.tsx
+++ app/tickets/create/page.tsx
@@ -1,10 +1,12 @@
-"use client";
-import { Button, TextField, Select } from "@radix-ui/themes";
+'use client'
+import { Button, TextField, Select, Callout } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { MdErrorOutline } from "react-icons/md";
 
 const CreateTicketPage = () => {
   interface TicketForm {
@@ -13,43 +15,64 @@ const CreateTicketPage = () => {
     priority: "LOW" | "MEDIUM" | "HIGH" | "CRITICAL";
   }
 
+  const [error, setError] = useState("");
+
   const { register, control, handleSubmit } = useForm<TicketForm>();
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <form
-      className="max-w-xl space-y-5"
-      onSubmit={ handleSubmit( async (data) => {
-        await axios.post(`/api/tickets`, data)
-        router.push(`/tickets`)
-      })}
-    >
-      <TextField.Root placeholder="Title" {...register("title")} />
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
+    <div className="max-w-xl">
+        
+      {error && (
+        <Callout.Root className="mb-3" color="red">
+        <Callout.Icon>
+        <MdErrorOutline />
+        </Callout.Icon>
+        <Callout.Text>{error}</Callout.Text>
+      </Callout.Root>
+      )}
+      <form className="space-y-5"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post(`/api/tickets`, data);
+            router.push(`/tickets`);
+          } catch (error) {
+            setError("Unexpected error occured.");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
 
-      <Controller
-        name="priority"
-        control={control}
-        render={({field}) => (
-          <Select.Root size="2" defaultValue="MEDIUM" value={field.value}  onValueChange={field.onChange}>
-            <Select.Trigger placeholder="Priority" />
-            <Select.Content>
-              <Select.Item value="LOW">Low</Select.Item>
-              <Select.Item value="MEDIUM">Medium</Select.Item>
-              <Select.Item value="HIGH">High</Select.Item>
-              <Select.Item value="CRITICAL">Critical</Select.Item>
-            </Select.Content>
-          </Select.Root>
-        )}
-      />
+        <Controller
+          name="priority"
+          control={control}
+          render={({ field }) => (
+            <Select.Root
+              size="2"
+              defaultValue="MEDIUM"
+              value={field.value}
+              onValueChange={field.onChange}
+            >
+              <Select.Trigger placeholder="Priority" />
+              <Select.Content>
+                <Select.Item value="LOW">Low</Select.Item>
+                <Select.Item value="MEDIUM">Medium</Select.Item>
+                <Select.Item value="HIGH">High</Select.Item>
+                <Select.Item value="CRITICAL">Critical</Select.Item>
+              </Select.Content>
+            </Select.Root>
+          )}
+        />
 
-      <Button>Add new ticket</Button>
-    </form>
+        <Button>Add new ticket</Button>
+      </form>
+    </div>
   );
 };
 
