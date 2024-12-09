d8ad807a7d9513da2bc62a40ed403a40d32dcb89
salmanpatnee <salmanpatni92@gmail.com>
2024-12-09 22:33:02 +0500

    Add the edit button

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { TicketPriorityBadge, TicketStatusBadge } from "@/app/components/";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading } from "@radix-ui/themes";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
 interface Props {
@@ -19,17 +21,27 @@ const TicketDetailPage = async ({ params }: Props) => {
   }
 
   return (
-    <div>
-      <Heading>{ticket.title}</Heading>
-      <Flex gap="3" my="2">
-        <TicketStatusBadge status={ticket.status} />
-        <TicketPriorityBadge priority={ticket.priority} />
-        <p>{ticket.createdAt.toDateString()}</p>
-      </Flex>
-      <Card className="prose" mt="5"> 
-        <ReactMarkdown>{ticket.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{initial: "1", md: "2"}} gap="5">
+      <Box>
+        <Heading>{ticket.title}</Heading>
+        <Flex gap="3" my="2">
+          <TicketStatusBadge status={ticket.status} />
+          <TicketPriorityBadge priority={ticket.priority} />
+          <p>{ticket.createdAt.toDateString()}</p>
+        </Flex>
+        <Card className="prose" mt="5"> 
+          <ReactMarkdown>{ticket.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+            <Pencil2Icon/>
+          <Link href={`/tickets/${ticket.id}/edit`}>
+            Edit issue
+          </Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
