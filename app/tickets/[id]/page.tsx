31fb5b01ec6985982d1b7992b9aaae018766de59
salmanpatnee <salmanpatni92@gmail.com>
2024-12-09 14:18:55 +0500

    Add markdown preview

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -4,32 +4,36 @@ import prisma from "@/prisma/client";
 import { Card, Flex, Heading } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   params: { id: string };
 }
 
 const TicketDetailPage = async ({ params }: Props) => {
-
   const ticket = await prisma.ticket.findUnique({
     where: {
       id: parseInt(params.id),
     },
   });
 
-  if(!ticket) {
-    notFound()
+  if (!ticket) {
+    notFound();
   }
 
-  return <div>
-    <Heading>{ticket.title}</Heading>
-    <Flex gap="3" my="2">
-      <TicketStatusBadge status={ticket.status}/>
-      <TicketPriorityBadge priority={ticket.priority}/>
-      <p>{ticket.createdAt.toDateString()}</p>
-    </Flex>
-    <Card>{ticket.description}</Card>
-  </div>;
+  return (
+    <div>
+      <Heading>{ticket.title}</Heading>
+      <Flex gap="3" my="2">
+        <TicketStatusBadge status={ticket.status} />
+        <TicketPriorityBadge priority={ticket.priority} />
+        <p>{ticket.createdAt.toDateString()}</p>
+      </Flex>
+      <Card className="prose" mt="5"> 
+        <ReactMarkdown>{ticket.description}</ReactMarkdown>
+      </Card>
+    </div>
+  );
 };
 
 export default TicketDetailPage;
