59ddc086bccea6f5c6f2f8eee9ef13b64abd7c9e
salmanpatnee <salmanpatni92@gmail.com>
2024-12-10 18:29:13 +0500

    Add a delete dialouge box

--- app/tickets/[id]/TicketDeleteButton.tsx
+++ app/tickets/[id]/TicketDeleteButton.tsx
@@ -1,13 +1,37 @@
-import {  TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+"use client";
+import { TrashIcon } from "@radix-ui/react-icons";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 
-const TicketDeleteButton = ({ticketId}: {ticketId: number}) => {
+const TicketDeleteButton = ({ ticketId }: { ticketId: number }) => {
   return (
-    <Button color="red">
-      <TrashIcon />
-      <Link href={`/tickets/${ticketId}/edit`}>Delete</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <TrashIcon />
+          Delete
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure? You want to delete this ticket?
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+    // <Link href={`/tickets/${ticketId}/edit`}>Delete</Link>
   );
 };
 
