d27ecf4ff232aaa132d15fe9c34bc329befdec4a
salmanpatnee <salmanpatni92@gmail.com>
2024-12-16 10:15:04 +0500

    Implement the pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from '@radix-ui/react-icons';
 import { Button, Flex, Text } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React from 'react';
 
 interface Props {
   itemCount: number;
@@ -12,8 +16,17 @@ const Pagination = ({
   pageSize,
   currentPage,
 }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount <= 1) return null;
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString());
+    router.push('?' + params.toString());
+  }
 
   return (
     <Flex align="center" gap="2">
@@ -24,6 +37,7 @@ const Pagination = ({
         color="gray"
         variant="soft"
         disabled={currentPage === 1}
+        onClick={() => changePage(1)}
       >
         <DoubleArrowLeftIcon />
       </Button>
@@ -31,6 +45,7 @@ const Pagination = ({
         color="gray"
         variant="soft"
         disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
       >
         <ChevronLeftIcon />
       </Button>
@@ -38,6 +53,7 @@ const Pagination = ({
         color="gray"
         variant="soft"
         disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
       >
         <ChevronRightIcon />
       </Button>
@@ -45,6 +61,7 @@ const Pagination = ({
         color="gray"
         variant="soft"
         disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
       >
         <DoubleArrowRightIcon />
       </Button>
